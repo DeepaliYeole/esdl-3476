3617203812a3e1d19e3271bd560ab8b8108b0be2
DeepaliYeole <deepali.yeole@cumminscollege.in>
2014-08-05 10:40:01 +0530

    Create calc.cpp
    my calc program

---
+++ calc.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+float a,b,c;
+cout<<"\n Enter two values";
+cin>>a>>b;
+cout<<"\n MENU:";
+cout<<"\n 1.Addition \n 2.Substraction \n 3.Multiplication \n 4.Division";
+cout<<"\n Enter your choice";
+cin>>d;
+switch(d)
+{
+case 1:
+ 
+}
+c=a+b;
+cout<<"\n Sum is:"<<c;
+return 0;
+}
